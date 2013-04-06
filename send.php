ccb1dc429fdb6594510cbdd0467294b2a230c568
Administrator <administrator@CesarBook.local>
2013-04-05 23:25:41 -0300

    Temporary bug

--- send.php
+++ send.php
@@ -1,13 +1,16 @@
 <?php
 
 //Put here the API token you get in the Pipedrive app
-$TOKEN = "";
+$TOKEN = "47bc4373557dd9ba1591902ec6aa1b928f0c6cdc";
 //This is the URL for persons in the Pipedrive app. It'll add a new person with name, mail and phone
 $url = "http://api.pipedrive.com/v1/persons?api_token=".$TOKEN;
 $jsonData = array(
-		"name"  => $_POST['name'],
-		"email" => array($_POST['email']),
-		"phone" => array($_POST['phone']),
+		"name"    => $_POST['name'],
+		"email"   => array($_POST['email']),
+		"phone"   => array($_POST['phone']),
+		"city"    => "NewCity",
+		"state"   => "NewState",
+		"message" => "NewMessage",
 );
 $jsonData = json_encode($jsonData);
 //Options for file_get_contents. We need to state method and content-type only.
